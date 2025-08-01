0d560d3cc0b63240ffe46bdda38e592af2d30c2e
Sou36 <135430197+Sou36@users.noreply.github.com>
2025-08-02 01:14:07 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ socket.onopen = () => {
 socket.onmessage = (event) => {
   const json = JSON.parse(event.data);
   console.log(json);
+
   if (json.uuid) {
     uuid = json.uuid;
   } else {
@@ -25,8 +26,33 @@ function sendMessage() {
     name: document.getElementById('nameInput').value,
     message: document.getElementById('msgInput').value,
     time: `${now.toLocaleDateString()} ${now.toLocaleTimeString()}`,
-    mine: true, // ← 自分のメッセージだとわかるように
   };
   socket.send(JSON.stringify(json));
   document.getElementById('msgInput').value = '';
+}
+
+function createMessage(json) {
+  const side = json.mine ? 'mine' : 'other';
+  const sideElement = createDiv(side);
+  const sideTextElement = createDiv(`${side}-text`);
+  const timeElement = createDiv('time');
+  const nameElement = createDiv('name');
+  const textElement = createDiv('text');
+
+  timeElement.textContent = json.time;
+  nameElement.textContent = json.name;
+  textElement.textContent = json.message;
+
+  sideTextElement.appendChild(timeElement);
+  sideTextElement.appendChild(nameElement);
+  sideTextElement.appendChild(textElement);
+  sideElement.appendChild(sideTextElement);
+
+  return sideElement;
+}
+
+function createDiv(className) {
+  const element = document.createElement('div');
+  element.classList.add(className);
+  return element;
 }
